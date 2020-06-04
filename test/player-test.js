e8dcac5e96d932bec1ffac3fd684703fcf9ffb02
Greyson Elkins <greysonelkins@gmail.com>
2020-06-04 17:35:06 -0600

    Start player test with property tests

---
+++ test/player-test.js
@@ -0,0 +1,36 @@
+const assert = require('chai').assert;
+const Player = require('../src/player');
+const Game = require('../src/game');
+
+describe('Player', () => {
+  it('should be a function', () => {
+    assert.isFunction(Player);
+  });
+
+  it ('should have an id', () => {
+   const player = new Player("1");
+
+   assert.equal(player.id, "1");
+  });
+
+  it('should start with no wins', () => {
+    const player = new Player("1");
+
+    assert.equal(player.wins, 0);
+  });
+
+  it('should start with an empty hand', () => {
+     const player = new Player("1");
+
+     assert.deepEqual(player.hand, [])
+  });
+
+  it.skip('should be able to play a card to the games central pile', () => {
+
+
+  });
+
+  it.skip('should be able to save to local storage', () => {
+
+  });
+});
