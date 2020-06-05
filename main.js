7111b9d5859c73877a8d5d94f0173e788155c25f
Greyson Elkins <greysonelkins@gmail.com>
2020-06-05 11:50:38 -0600

    Assign event keycodes

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-//make sure event listeners can check players turn
+
+window.addEventListener('keydown', handleKeydown);
+
+function handleKeydown(event) {
+  if (event.which == 81) {
+    //player 1 play card
+    console.log("Q");
+  } else if (event.which == 70) {
+    //player 1 slap
+    console.log("F")
+  } else if (event.which == 80) {
+    //player 2 play card
+    console.log("P")
+  } else if (event.which == 74) {
+    //player 2 slap
+    console.log("J")
+  }
+}
