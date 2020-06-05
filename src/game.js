c612ed7e3f8fc12fa4000d6e42a685663edf082e
Greyson Elkins <greysonelkins@gmail.com>
2020-06-04 19:34:08 -0600

    Finish writing player test

---
+++ src/game.js
@@ -0,0 +1,1 @@
+// players save to local.storage, game can check local storage and recieve them 
--- test/player-test.js
+++ test/player-test.js
@@ -22,15 +22,22 @@ describe('Player', () => {
   it('should start with an empty hand', () => {
      const player = new Player("1");
 
-     assert.deepEqual(player.hand, [])
+     assert.deepEqual(player.hand, []);
   });
 
   it.skip('should be able to play a card to the games central pile', () => {
+    const game = new Game();
+    game.player1.hand = ["king"];
+    game.player1.playCard();
 
+    assert.deepEqual(game.centerPile, ["king"]);
+    assert.deepEqual(game.player1.hand, []);
 
   });
 
-  it.skip('should be able to save to local storage', () => {
+  it.skip('save to local storage should be a function', () => {
+    const 
 
+    assert.isFunction()
   });
 });
