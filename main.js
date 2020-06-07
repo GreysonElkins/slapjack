afabb8c9dfd2f73f8a68f791deaecb2c7c8fe2b3
Greyson Elkins <greysonelkins@gmail.com>
2020-06-06 19:05:25 -0600

    Refactor event handler

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
 var game = new Game();
-var player1 = game.player1
-var player2 = game.player2
-var keypress = event.which
+var pageCenterPile = document.querySelector('.center-pile')
 
 window.onload = game.setGame();
 
 window.addEventListener('keydown', handleKeydown);
 
 function handleKeydown(event) {
-  if (keypress == 81) {
-    game.movePlayersCard(game.player1);
-    placeCard(player1);
-  } else if (keypress == 70) {
-    game.slap(player1);
+  var player;
+  var keypress = event.which
+  if (keypress == 81 || keypress == 70) {
+    player = game.player1
+  } else if (keypress == 80 || keypress == 74) {
+    player = game.player2;
+  }
+  gameHandler(player, keypress);
+}
 
-  } else if (keypress == 80) {
-    game.movePlayersCard(game.player2);
-    placeCard(player1);
-  } else if (keypress == 74) {
-    game.slap(player2);
+function gameHandler(player, keypress) {
+  if (keypress == 81 || keypress == 80) {
+    game.movePlayersCard(player);
+    showCard(player);
+  } else if (keypress == 70 || keypress == 74) {
+    game.slap(player);
+    removePile();
+    wipeHand(player);
   }
 }
 
-function gameHandler(player, event) {
-  if (keypress == 81 || keypress == 80) {
-    game.movePlayersCard(player);
-    placeCard(player);
-  } 
-}
-
-function placeCard(player) {
+function showCard(player) {
   var color;
-  var centerCard = document.getElementById('center-card');
 
-  if (player == game.player1) {
+debugger
+  if (player.id == "1") {
     color = "#009FB7";
   } else {
     color = "#fFD23F";
   }
 
   if (game.centerPile[0] !== undefined) {
+
   pageCenterPile.innerHTML =
     `<img src="${game.centerPile[0].src}" alt="${game.centerPile[0].suit}
     ${game.centerPile[0].type}" id="center-card" />`;
-  centerCard.style.boxShadow = `0 0 13px 0px ${color}`;
+    
+  document.getElementById('center-card').style.boxShadow = `0 0 13px 0px ${color}`;
   }
 
   wipeHand(player);
 }
 
 function removePile() {
-  var pageCenterPile = document.querySelector('.center-pile')
   if (game.centerPile == []) {
   pageCenterPile.innerHTML = "";
   }
@@ -58,6 +57,6 @@ function removePile() {
 
 function wipeHand(player){
   if (player.hand == []) {
-    document.getElementById(`.${player}-hand`).classList.add.('.hidden')
+    document.getElementById(`.${player}-hand`).classList.add('.hidden');
   }
 }
